7525d8ac539079ee83290eb5ddfb8e8b5ecb449a
Michał Jastrzębowski <michal.jastrzebowski@gmail.com>
2017-06-29 10:46:24 +0200

    angular-webpack2-starter init

---
+++ src/app/app.imports.ts
@@ -0,0 +1,40 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IdlePreload, IdlePreloadModule } from '@angularclass/idle-preload';
+
+import { EffectsModule } from '@ngrx/effects';
+import { RouterStoreModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { useLogMonitor } from '@ngrx/store-log-monitor';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { rootReducer } from './reducers';
+import { StoreDevToolsModule } from './features/store-devtools.module';
+import { UserEffects } from './user/user.effects';
+
+const STORE_DEV_TOOLS_IMPORTS = [];
+if (ENV === 'development' && !AOT &&
+  ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
+) STORE_DEV_TOOLS_IMPORTS.push(...[
+  StoreDevtoolsModule.instrumentStore({
+    monitor: useLogMonitor({
+      visible: true,
+      position: 'right'
+    })
+  })
+]);
+
+export const APP_IMPORTS = [
+  BrowserAnimationsModule,
+  EffectsModule.run(UserEffects),
+  NgbModule.forRoot(),
+  ReactiveFormsModule,
+  RouterStoreModule.connectRouter(),
+  StoreModule.provideStore(rootReducer),
+  STORE_DEV_TOOLS_IMPORTS,
+  StoreDevToolsModule
+];
+
